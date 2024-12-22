cdef1a8fec3c13277819a4bc3bbb3e08e28c984f
pedroelir <pedro_r_b777@hotmail.com>
2024-12-22 22:01:18 +0100

    run black formatter logger_conf

--- src/KMI/utils/logger_conf.py
+++ src/KMI/utils/logger_conf.py
@@ -7,36 +7,35 @@ from pathlib import Path
 
 _logs_path = Path.cwd() / "logs"
 _now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S_%f")
-STANDARD_FORMAT = '%(asctime)s [%(name)s] [%(levelname)s] %(message)s'
+STANDARD_FORMAT = "%(asctime)s [%(name)s] [%(levelname)s] %(message)s"
 VERBOSE_FORMAT = "%(asctime)s [%(filename)s:%(lineno)s - %(funcName)s] [%(levelname)s] %(message)s"
 
 LOGGING_CONFIG = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': VERBOSE_FORMAT,
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": VERBOSE_FORMAT,
         },
-
-        'standard': {
-            'format': STANDARD_FORMAT,
-        },
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'standard',
-        },
-        'file': {
-            'class': 'logging.FileHandler',
-            'formatter': 'verbose',
-            'filename': f'{_logs_path}/KMI_{_now}.log',
+        "standard": {
+            "format": STANDARD_FORMAT,
         },
     },
-    'loggers': {
-        'KMI': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG',
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+        "file": {
+            "class": "logging.FileHandler",
+            "formatter": "verbose",
+            "filename": f"{_logs_path}/KMI_{_now}.log",
+        },
+    },
+    "loggers": {
+        "KMI": {
+            "handlers": ["console", "file"],
+            "level": "DEBUG",
             # 'propagate': False, # Do not propagate to root logger
         },
     },
@@ -46,6 +45,7 @@ _logs_path.mkdir(parents=True, exist_ok=True)
 logging.config.dictConfig(LOGGING_CONFIG)
 _global_logger: logging.Logger = logging.getLogger(__package__)
 logger = _global_logger
+
 
 def get_global_logger() -> logging.Logger:
     """Return the global logger object."""
