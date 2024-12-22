0c52e631ff258bcc95c371462109cabd150f2ce8
pedroelir <pedro_r_b777@hotmail.com>
2024-12-22 22:00:46 +0100

    add option to get child file logger

--- src/KMI/utils/logger_conf.py
+++ src/KMI/utils/logger_conf.py
@@ -44,9 +44,35 @@ LOGGING_CONFIG = {
 
 _logs_path.mkdir(parents=True, exist_ok=True)
 logging.config.dictConfig(LOGGING_CONFIG)
-_global_logger: logging.Logger = logging.getLogger('KMI')
+_global_logger: logging.Logger = logging.getLogger(__package__)
 logger = _global_logger
 
 def get_global_logger() -> logging.Logger:
     """Return the global logger object."""
     return _global_logger
+
+
+def get_child_file_logger(name: str) -> logging.Logger:
+    """
+    Creates and returns a child logger with a file handler.
+    This function creates a child logger from a global logger, adds a file handler to it,
+    and sets a formatter for the file handler. The log file will be named using the provided
+    name and the current timestamp.
+
+    Example:
+        ```python
+        from KMI.utils.logger_conf import get_child_file_logger
+        child_logger = get_child_file_logger("exam")
+        child_logger.info("This is a log message.") # This message will be logged to a file named "exam_<timestamp>.log"
+        ```
+    :param name: The name of the child logger.
+    :return: A child logger with a file handler.
+    """
+
+    file_hdlr = logging.FileHandler(f"{_logs_path}/{name}_{_now}.log")
+    file_formatter = logging.Formatter(VERBOSE_FORMAT)
+    file_hdlr.setFormatter(file_formatter)
+    child_logger: logging.Logger = _global_logger.getChild(name)
+    child_logger.addHandler(file_hdlr)
+
+    return child_logger
